336eb234cbd79f98f41ea3043cd88a512b7975b8
Daria <jadarya@mail.ru>
2021-02-10 07:46:12 +0900

    add facebook login

--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -1,10 +1,25 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { LoginButton, AccessToken } from 'react-native-fbsdk';
 
 export default function LoginScreen() {
   return (
     <View style={styles.root}>
       <Text>Dasha</Text>
+      <LoginButton
+        onLoginFinished={(error, result) => {
+          if (error) {
+            console.log('login has error: ' + result.error);
+          } else if (result.isCancelled) {
+            console.log('login is cancelled.');
+          } else {
+            AccessToken.getCurrentAccessToken().then((data) => {
+              console.log(data.accessToken.toString());
+            });
+          }
+        }}
+        //onLogoutFinished={() => console.log("logout.")}/>
+      />
     </View>
   );
 }
